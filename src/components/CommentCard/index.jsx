4fa25532594b4cc2bc1301355fca1012cea19a39
purwar1997 <shubhampurwar511@gmail.com>
2023-06-20 01:11:36 +0530

    changes saved

--- src/components/CommentCard/index.jsx
+++ src/components/CommentCard/index.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BsThreeDots } from 'react-icons/bs';
+import { FaTrashAlt, FaPen } from 'react-icons/fa';
 import './index.css';
 
 export default function CommentCard({ comment }) {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+
   const formatHeadline = headline =>
     headline.length > 120 ? `${headline.slice(0, 120)}...` : headline;
 
@@ -15,13 +19,39 @@ export default function CommentCard({ comment }) {
         {comment.addedBy.isAuthor && (
           <>
             <span className='comment-author'>Author</span>
-            <BsThreeDots className='three-dots' />
+            <BsThreeDots className='three-dots' onClick={() => setIsPopupOpen(!isPopupOpen)} />
           </>
         )}
       </div>
 
       <p className='comment-headline'>{formatHeadline(comment.addedBy.headline)}</p>
-      <p className='comment-text'>{comment.text}</p>
+
+      {isEdit ? (
+        <div className='comment-edit-form'>
+          <textarea name='comment' placeholder='Add a comment...' value={comment.text} onChange={} />
+
+          <div className='btn-group'>
+            <button onClick={}>Save Changes</button>
+            <button onClick={() => setIsEdit(false)}>Cancel</button>
+          </div>
+        </div>
+      ) : (
+        <p className='comment-text'>{comment.text}</p>
+      )}
+
+      {isPopupOpen && (
+        <div className='comment-popup'>
+          <div onClick={() => setIsEdit(true)}>
+            <FaPen />
+            <span>Edit</span>
+          </div>
+
+          <div>
+            <FaTrashAlt />
+            <span>Delete</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
